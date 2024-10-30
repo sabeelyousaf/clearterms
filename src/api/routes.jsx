10808b464122eaf5ddac84a9496b0cb02e6ab91d
sabeelydev <sabeel.y@sevenkoncepts.com>
2024-10-31 03:40:55 +0500

    full-code

--- src/api/routes.jsx
+++ src/api/routes.jsx
@@ -1,22 +1,22 @@
 //Auth
-const login = "http://api.shaheendrives.com/api/login"
-const register = "http://api.shaheendrives.com/api/register"
-const profileUpdate = "http://api.shaheendrives.com/api/profile-update"
+const login = "https://api.shaheendrives.com/api/login"
+const register = "https://api.shaheendrives.com/api/register"
+const profileUpdate = "https://api.shaheendrives.com/api/profile-update"
 
 
 //Stripe
-const checkSubscription="http://api.shaheendrives.com/api/check-subscription"
-const checkoutSession="http://api.shaheendrives.com/api/create-checkout-session"
+const checkSubscription="https://api.shaheendrives.com/api/check-subscription"
+const checkoutSession="https://api.shaheendrives.com/api/create-checkout-session"
 
 //Docs
-const allDocs="http://api.shaheendrives.com/api/docs"
-const singleDoc="http://api.shaheendrives.com/api/doc"
-const uploadDoc="http://api.shaheendrives.com/api/doc/upload"
+const allDocs="https://api.shaheendrives.com/api/docs"
+const singleDoc="https://api.shaheendrives.com/api/doc"
+const uploadDoc="https://api.shaheendrives.com/api/doc/upload"
 
 
 //AI
-const docSummarize="http://api.shaheendrives.com/api/doc/summarize"
-const docSimplify="http://api.shaheendrives.com/api/doc/simplify"
+const docSummarize="https://api.shaheendrives.com/api/doc/summarize"
+const docSimplify="https://api.shaheendrives.com/api/doc/simplify"
 
 
 
